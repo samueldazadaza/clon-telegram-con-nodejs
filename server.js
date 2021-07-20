4e54e3506af1e0b1824c5f109ab0c711f09d906c
Samuel Daza <samueldazadaza@gmail.com>
2021-07-19 21:24:28 -0500

    ¿Cómo pueden venir las peticiones?7/33

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function (req, res) {
-    res.send('Hola');
+app.use(router);
+
+router.get('/message', function (req, res) {
+    res.send('Lista de mensajes');
 });
 
+router.post('/message', function (req, res) {
+    res.send('Mensaje añadido');
+});
+
+// app.use('/', function (req, res) {
+//     res.send('Hola');
+// });
+
 app.listen(3000);
-console.log('La aplicacion esta escuchando en: http://localhost:3000')
+console.log('La aplicacion esta escuchando en: http://localhost:3000');
